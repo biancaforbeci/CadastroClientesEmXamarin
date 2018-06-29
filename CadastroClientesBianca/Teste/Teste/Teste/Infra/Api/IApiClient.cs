924ef50fd0276629ee311c1f20301d913eaca305
bianca forbeci michaloskei <bianca.michaloskei@lactec.org.br>
2018-06-29 13:02:06 -0300

    WIP: #0000539 - Começo de implementação de API

---
+++ CadastroClientesBianca/Teste/Teste/Teste/Infra/Api/IApiClient.cs
@@ -0,0 +1,16 @@
+﻿using AppClientes.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppClientes.Infra.Api
+{
+    public interface IApiClient : IDisposable
+    {
+        Task<List<Client>> GetAsync<T>(string apiRoute, Action<Task<List<Client>>> callback = null);
+        IApiClient UseSufix(string urlSufix);
+        Task<List<Client>> PostResultAsync<Client>(string apiRoute, object body = null, Action<Task<List<Client>>> callback = null);
+        Task<string[]> PostAsync<Client>(string apiRoute, object body = null, Action<Task<string[]>> callback = null);
+    }
+}
