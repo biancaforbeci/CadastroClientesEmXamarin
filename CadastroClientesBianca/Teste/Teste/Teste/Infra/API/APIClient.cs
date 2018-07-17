25cf9f80aa54b9b0c0df099683209cb9e02be41b
bianca forbeci michaloskei <bianca.michaloskei@lactec.org.br>
2018-07-17 13:48:22 -0300

    WIP: Arrumando método de Post.

--- CadastroClientesBianca/Teste/Teste/Teste/Infra/API/APIClient.cs
+++ CadastroClientesBianca/Teste/Teste/Teste/Infra/API/APIClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ using AppClientes.Infra.Services;
 using AppClientes.Models;
 using MyCouch;
 using Newtonsoft.Json;
+using Plugin.Media.Abstractions;
 using Prism.Services;
 
 namespace AppClientes.Infra.Api
@@ -24,7 +26,10 @@ namespace AppClientes.Infra.Api
 
         IPageDialogService _pageDialog;
         public string content { get; set; }
-        protected HttpClient modernHttpClient { get; set;}
+        protected HttpClient modernHttpClient { get; set; }
+        private static string BaseCouchDbApiAddress = "http://localhost:5984";
+        private static HttpClient Client = new HttpClient() { BaseAddress = new Uri(BaseCouchDbApiAddress) };
+        private string sContentType = "application/json";
 
         public async Task<IEnumerable<Client>> GetAsync(string apiRoute)
         {
@@ -33,7 +38,7 @@ namespace AppClientes.Infra.Api
                 var uri = new Uri(string.Format(apiRoute, string.Empty));
 
                 var response = await API_Singleton.Instance.GetAsync(uri);
-                
+
                 if (response.IsSuccessStatusCode)
                 {
                     content = await response.Content.ReadAsStringAsync();
@@ -55,11 +60,11 @@ namespace AppClientes.Infra.Api
         public string Read_JSON()
         {
             return content;
-        }      
-        
+        }
+
         public string CompressionGZIPAsync(string json)
-        {               
-           return CompressionGZIP.CompressString(json);
+        {
+            return CompressionGZIP.CompressString(json);
         }
 
         private void InitHttpClient(HttpClient client)
@@ -70,10 +75,24 @@ namespace AppClientes.Infra.Api
 
         public async void PostAsync(string files)
         {
-            using (var client = new MyCouchClient("http://localhost:5984/", "soapui"))
+            try
             {
-                await client.Documents.PostAsync(content);
-            }                
+                var uri = new Uri(string.Format("http://localhost:5984/my_db", string.Empty));
+                var authResult = await Client.PostAsync(uri, new StringContent(files, Encoding.UTF8, sContentType));
+
+                if (authResult.IsSuccessStatusCode)
+                {
+                    await _pageDialog.DisplayAlertAsync("Sucesso", " ", "OK");
+                }
+            }
+            catch (Exception e)
+            {
+                await _pageDialog.DisplayAlertAsync("Erro", "Erro: " + e, "OK");
+            }
         }
-    }
-}
+    }       
+ }
+
+
+
+
